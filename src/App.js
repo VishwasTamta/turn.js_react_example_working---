02b4340ee2688c05429bf80eb8c9c8bb5e8d4662
Vishwas Tamta <vishwastamta@gmail.com>
2023-04-20 19:36:15 +0530

    added pdf dynamically

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "turn.js";
 
 import "./styles.css";
 import Turn from "./Turn.js";
+import { useEffect, useState } from "react";
 
 const options = {
   width: 800,
@@ -19,25 +20,41 @@ const options = {
   }
 };
 
-const pages = [
-  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/01.jpg",
-  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/02.jpg",
-  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/03.jpg",
-  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/04.jpg",
-  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/05.jpg",
-  "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/06.jpg"
-];
+// const pages = [
+//   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/01.jpg",
+//   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/02.jpg",
+//   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/03.jpg",
+//   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/04.jpg",
+//   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/05.jpg",
+//   "https://raw.github.com/blasten/turn.js/master/demos/magazine/pages/06.jpg"
+// ];
 
 function App() {
+  const [pages, setPages] = useState([]);
 
-  return (
-    <Turn options={options} className="magazine">
-      {pages.map((page, index) => (
-        <div key={index} className="page">
-          <img src={page} alt="" />
-        </div>
-      ))}
-    </Turn>
+  useEffect(() => {
+    const getImages = async () => {
+      const response = await fetch('http://localhost:3000/getPDF', {
+        method: 'GET'
+      })
+      const data = await response.json()
+      setPages(data.images)
+    }
+    getImages()
+  }, [])
+
+  return (<>{pages.length > 0 &&
+    <div className="magHolder">
+      <Turn options={options} className="magazine">
+        {pages?.map((page, index) => {
+          return <div key={index} className="page">
+            <img src={'http://localhost:3000/output/pdf.pdf/' + page} alt="" />
+          </div>
+        })}
+      </Turn>
+    </div>
+  }
+  </>
   );
 }
 
